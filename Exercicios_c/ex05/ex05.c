229ca3142153e7f6f3e2be2e67a4414c7dcbb0cf
Rafael Vizu <68239414+rafaelvizu@users.noreply.github.com>
2022-03-30 23:44:45 -0300

    add

--- Exercicios_c/ex05/ex05.c
+++ Exercicios_c/ex05/ex05.c
@@ -4,17 +4,20 @@
 
 int main() {
     float valor, lucro;
+    int porcentagemLucro;
 
     printf("Valor produto: R$");
     scanf("%f", &valor);
 
     if (valor<200) {
         lucro = (valor * 50) / 100;
+        porcentagemLucro = 50;
     } else {
         lucro = (valor * 45) / 100;
+        porcentagemLucro = 45;
     }
 
-    printf("\nValor original: R$%0.2f\tLucro: +R$%0.2f\t\tTotal = R$%0.2f\n\n", valor, lucro, (valor+lucro));
+    printf("\nValor original: R$%0.2f\tLucro de %d%%: +R$%0.2f\t\tTotal = R$%0.2f\n\n", valor, porcentagemLucro, lucro, (valor+lucro));
 
     system("pause");
     
