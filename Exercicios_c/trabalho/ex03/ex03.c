cf28349d4fb694458d86dfd20b5ab9fbc67b2894
Rafael Vizu <68239414+rafaelvizu@users.noreply.github.com>
2022-06-08 14:38:17 -0300

    kk

--- Exercicios_c/trabalho/ex03/ex03.c
+++ Exercicios_c/trabalho/ex03/ex03.c
@@ -2,27 +2,42 @@
 
 
 int main() {
-     char *matricula;
+     char matricula[50];
 
      printf("Digite o numero de matricula: ");
      scanf("%s", matricula);
 
      printf("\n\nNumero de matricula: %s\n\n", matricula);
 
-     char *algorismo;
+     char algorismo;
 
-     printf("Digite um dos quatros algorismos da sua matricula: ");
-     scanf("%s", &algorismo);
-     
+     printf("Digite um dos primeiros quatros algorismos da sua matricula: ");
+     fflush(stdin);
+     scanf("%c", &algorismo);
+
+
+     float n1, n2;
+
+     printf("\nDigite um numero: ");
+     scanf("%f", &n1);
+
+     printf("Digite mais um numero: ");
+     scanf("%f", &n2);
+
+     printf("\n");
      if (algorismo == matricula[3])
      {
-          /* code */
+          printf("Adicao: %0.1f", n1+n2);
      } else if (algorismo == matricula[1])
-     {
-          /* code */
+     { 
+          printf("Subtracao: %0.1f", n1-n2);
      } else if (algorismo == matricula[4])
      {
-          /* code */
+          printf("Multiplicacao: %0.1f", n1*n2);
+     } else if (algorismo == matricula[2]) {
+          printf("Divisao: %0.1f", n1/n2);
+     } else if (algorismo == matricula[0]) {
+          printf("Exit...");
      }
      
      
---
+++ Exercicios_c/trabalho/ex04/ex04.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+int main() {
+     int arrNum[4][2];
+     int maior, menor;
+     int posMaior[2];
+     int posMenor[2];
+
+     for (int i = 0; i < 4; i++)
+     {
+          printf("\nColuna: %d\n\n", i);
+          for (int f = 0; f < 2; f++)
+          {
+               printf("Digite o valor [%d][%d]: ",i,f);
+               scanf("%d", &arrNum[i][f]);
+
+               if (i==0)
+               {
+                    menor = arrNum[i][f];
+                    maior = arrNum[i][f];
+                    posMaior[0] = i; posMaior[1] = f;
+                    posMenor[0] = i; posMenor[1] = f;
+               } else if (arrNum[i][f] > maior) {
+                    maior = arrNum[i][f];
+                    posMaior[0] = i; posMaior[1] = f;
+               } else if (arrNum[i][f] < menor) {
+                    menor = arrNum[i][f];
+                    posMenor[0] = i; posMenor[1] = f;
+               }
+               
+          }
+          printf("\n");
+     }
+
+
+     printf("\n\nMaior:\t\t %d", maior);
+     printf("\nPosicao:\t [%d][%d]\n\n", posMaior[0], posMaior[1]);
+
+     printf("Menor:\t\t %d", menor);
+     printf("\nPosicao:\t [%d][%d]", posMenor[0], posMenor[1]);
+     
+
+     return 0;
+}
