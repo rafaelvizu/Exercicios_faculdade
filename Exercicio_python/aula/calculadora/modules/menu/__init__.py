151433bb4fb72615e6cd73ba2fd70de319a43a95
Rafael Vizú <rafaelvizu2@gmail.com>
2023-03-09 21:08:39 -0300

    Update __init__.py

--- Exercicio_python/aula/calculadora/modules/menu/__init__.py
+++ Exercicio_python/aula/calculadora/modules/menu/__init__.py
@@ -7,7 +7,7 @@ def clear():
 def menu():
      print('CALCULADORA\n\n')
 
-     print('1. Sumar')
+     print('1. Somar')
      print('2. Subtrair')
      print('3. Multiplicar')
      print('4. Dividir')
