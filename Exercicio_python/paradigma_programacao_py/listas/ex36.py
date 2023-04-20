4e419153e917eca407da5f1ec16c13d7957daa59
Rafael Vizú <rafaelvizu2@gmail.com>
2023-04-20 17:51:20 -0300

    upd

--- Exercicio_python/paradigma_programacao_py/listas/ex36.py
+++ Exercicio_python/paradigma_programacao_py/listas/ex36.py
@@ -2,10 +2,10 @@ from time import sleep
 
 
 def fatorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * fatorial(n - 1)
+     if n == 0:
+          return 1
+     else:
+          return n * fatorial(n - 1)
     
 
 while True:
